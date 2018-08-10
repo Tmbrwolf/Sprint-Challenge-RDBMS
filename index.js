a0f314bdb3dd006db5b2b076f2c6f2cbb9416d03
Eric B <timberwolf.eric@gmail.com>
2018-08-10 11:54:11 -0500

    Update (put) & Delete methods added for projects

--- index.js
+++ index.js
@@ -39,6 +39,32 @@ server.get('/projects/:id', (req, res) => {
     });
 });
 
+server.put('/projects/:id', (req, res) => {
+  const { id } = req.params;
+  const project = req.body;
+  db('projects')
+    .where("id", id)
+    .update(project)
+    .then(project => {
+      res.status(201).json({project});
+    })
+    .catch(error => res.status(500).json({error}));
+});
+
+server.delete('/projects/:id', (req, res) => {
+  const { id } = req.params;
+  db('projects')
+    .where({ id })
+    .del()
+    .then(response => {
+      res.status(200).json(`${id} is deleted.`);
+    })
+    .catch(error => {
+      res.status(500).json({error});
+    });
+});
+
+
 server.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
 })
