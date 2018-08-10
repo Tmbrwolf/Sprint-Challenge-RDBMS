28f86a39206f8fd2a8228dca4ef3ddc388265184
Eric B <timberwolf.eric@gmail.com>
2018-08-10 11:36:57 -0500

    Get & Post methods added for projects

--- index.js
+++ index.js
@@ -6,6 +6,27 @@ server.use(express.json());
 
 const PORT = 8000;
 
+server.get('/projects', (req, res) => {
+  db('projects')
+  .then(response => {
+    res.status(200).json(response);
+  })
+  .catch(error => {
+    res.status(500).json(error);
+  });
+});
+
+server.post('/projects', (req, res) => {
+  const project = req.body;
+  db.insert(project).into('projects')
+    .then(response => {
+      res.status(200).json({project});
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
 server.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
 })
